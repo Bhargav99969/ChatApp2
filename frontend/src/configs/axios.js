c2e5cafd846e5408934c108618bff97975c1862d
Bhargav99969 <unthale@gmail.com>
2025-07-05 17:41:48 +0530

    e

--- frontend/src/configs/axios.js
+++ frontend/src/configs/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL:["https://chatapp2backend.onrender.com","http://localhost:3000/"],
+    baseURL:["https://chatapp2backend.onrender.com","http://localhost:3000/api"],
     withCredentials:true
 })
